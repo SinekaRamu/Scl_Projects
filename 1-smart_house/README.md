d8953958113a5b05f641771cba83a649e17230fb
SinekaRamu <sineka2001cud@gmail.com>
2025-02-26 17:04:19 +0530

    feb - 26

--- 1-smart_house/README.md
+++ 1-smart_house/README.md
@@ -9,3 +9,27 @@ IoT-based home automation enables smart control of household appliances using in
 - Communication Modules – WiFi for connectivity.
 - Cloud Platform – Blynk for remote access and data storage.
 - Mobile/Web App – Blynk App For real-time monitoring and control.
+
+## Components Used:
+
+- ESP8266 NodeMCU
+- 2-Channel Relay
+- Servo Motor
+- Dc Motor with Fan
+- LED (12V)
+- Multi Output power module
+- Adapter
+
+## Connection
+
+| Node MCU | Components |
+| -------- | ---------- |
+| D1       | Relay Fan  |
+| D2       | Relay LED  |
+| D7       | Servo      |
+| Vin      | Multi 5V   |
+| Gnd      | Gnd        |
+
+- DC motor is connected with relay with 3.3 V Supply
+- LED is connected with relay with 12 V supply
+- Node MCU is power via Vin with 5V.
--- 2-U_turn_Obstacle_detection/code/2-Traffic_U_turn.ino
+++ 2-U_turn_Obstacle_detection/code/2-Traffic_U_turn.ino
@@ -1,32 +1,34 @@
 //@Ediylabs
-const int trig1=8;
-const int trig2=9;
-const int echno1=7;
-const int echno2=10;
-const int red1=6;
-
-const int red2=13;
-const int yel1=5;
-const int yel2=12;
-const int gr1=4;
-const int gr2=11;
 const int buzzer = 3;
+const int trig1 = 8;
+const int trig2 = 9;
+const int echno1 = 7;
+const int echno2 = 10;
+const int red1 = 6;
 
-void setup() {
-    pinMode(trig1,OUTPUT);
-    pinMode(trig2,OUTPUT);
-    pinMode(echno1,INPUT);
-    pinMode(echno2,INPUT);
-    pinMode(red1,OUTPUT);
-    pinMode(red2,OUTPUT);
-    pinMode(yel1,OUTPUT);
-    pinMode(yel2,OUTPUT);
-    pinMode(gr1,OUTPUT);
-    pinMode(gr2,OUTPUT);
+const int red2 = 13;
+const int yel1 = 5;
+const int yel2 = 12;
+const int gr1 = 4;
+const int gr2 = 11;
+
+void setup()
+{
+    pinMode(trig1, OUTPUT);
+    pinMode(trig2, OUTPUT);
+    pinMode(echno1, INPUT);
+    pinMode(echno2, INPUT);
+    pinMode(red1, OUTPUT);
+    pinMode(red2, OUTPUT);
+    pinMode(yel1, OUTPUT);
+    pinMode(yel2, OUTPUT);
+    pinMode(gr1, OUTPUT);
+    pinMode(gr2, OUTPUT);
     pinMode(buzzer, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
     digitalWrite(trig1, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig1, LOW);
@@ -34,53 +36,68 @@ void loop() {
     int distance = duration * 0.034 / 2;
 
     // Check distance and alert drivers
-    if (distance <7) { // Adjust distance threshold
+    if (distance < 7)
+    {                             // Adjust distance threshold
         digitalWrite(red2, HIGH); // Red LED indicates danger
         digitalWrite(yel2, LOW);
         digitalWrite(gr2, LOW);
-    } else if (distance <20) {
+    }
+    else if (distance < 20)
+    {
         digitalWrite(red2, LOW);
         digitalWrite(yel2, HIGH); // Yellow LED indicates caution
         digitalWrite(gr2, LOW);
-    } else {
+    }
+    else
+    {
         digitalWrite(red2, LOW);
         digitalWrite(yel2, LOW);
         digitalWrite(gr2, HIGH); // Green LED indicates safe distance
     }
-    
+
     digitalWrite(trig2, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig2, LOW);
-    int duration2= pulseIn(echno2, HIGH);
-    int distance2= duration2 * 0.034 / 2;
+    int duration2 = pulseIn(echno2, HIGH);
+    int distance2 = duration2 * 0.034 / 2;
 
     // Check distance and alert drivers
-    if (distance2<7) { // Adjust distance threshold
+    if (distance2 < 7)
+    {                             // Adjust distance threshold
         digitalWrite(red1, HIGH); // Red LED indicates danger
         digitalWrite(yel1, LOW);
         digitalWrite(gr1, LOW);
-    } else if (distance2<20) {
+    }
+    else if (distance2 < 20)
+    {
         digitalWrite(red1, LOW);
         digitalWrite(yel1, HIGH); // Yellow LED indicates caution
         digitalWrite(gr1, LOW);
-    } else {
+    }
+    else
+    {
         digitalWrite(red1, LOW);
         digitalWrite(yel1, LOW);
         digitalWrite(gr1, HIGH); // Green LED indicates safe distance
     }
 
-    if(distance <7 && distance2<7){
-      digitalWrite(3, HIGH);
-      delay(50);
-      digitalWrite(3, LOW);
-      delay(50);
-    }else if(distance <7 || distance2<7){
-      digitalWrite(3, HIGH);
-      delay(500);
-      digitalWrite(3, LOW);
-      delay(500);
-    }else{
-      digitalWrite(3, LOW);
+    if (distance < 7 && distance2 < 7)
+    {
+        digitalWrite(3, HIGH);
+        delay(50);
+        digitalWrite(3, LOW);
+        delay(50);
+    }
+    else if (distance < 7 || distance2 < 7)
+    {
+        digitalWrite(3, HIGH);
+        delay(500);
+        digitalWrite(3, LOW);
+        delay(500);
+    }
+    else
+    {
+        digitalWrite(3, LOW);
     }
     delay(100);
 }
